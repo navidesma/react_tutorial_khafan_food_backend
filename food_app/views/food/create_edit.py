5cb56fb58d8a9a84a294cbe3a6f388388a3a1050
navidesma <esma.navid@gmail.com>
2024-07-07 00:46:41 +0330

    fixed create food

--- food_app/views/food/create_edit.py
+++ food_app/views/food/create_edit.py
@@ -1,6 +1,9 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.parsers import MultiPartParser, FormParser
 
 from food_app.models.food import Food
 from food_app.serializers.food import FoodSerializer
@@ -11,13 +14,25 @@ class FoodViewSetWithoutPermission(ModelViewSet):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     permission_classes = [IsAuthenticated]
-    
+
 
 class FoodViewSet(ModelViewSet):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
     permission_classes = [IsRestaurantOwnerPermission]
+    parser_classes = (MultiPartParser, FormParser)
 
     def check_object_permissions(self, request, obj):
         if obj.restaurant != request.user.profile.restaurant:
             raise PermissionDenied()
+
+    def create(self, request, *args, **kwargs):
+        request_data = self.request.data
+        request_data["restaurant"] = request.user.profile.restaurant.id
+        file_serializer = self.get_serializer(data=request_data)
+        file_serializer.is_valid(raise_exception=True)
+        self.perform_create(file_serializer)
+        headers = self.get_success_headers(file_serializer.data)
+        return Response(
+            file_serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
