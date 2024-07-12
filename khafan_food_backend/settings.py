38fec1756fb3ab6818af12def76f41da7ca6c0a4
navidesma <esma.navid@gmail.com>
2024-07-13 00:48:02 +0330

    changed page size

--- khafan_food_backend/settings.py
+++ khafan_food_backend/settings.py
@@ -39,7 +39,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 10,
+    "PAGE_SIZE": 3,
     "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
